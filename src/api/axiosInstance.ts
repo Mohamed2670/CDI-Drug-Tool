ec40b2a1d74226d44a5152e5c5b5e424f1589198
Mohamed2670 <mh639450@gmail.com>
2025-07-14 00:46:41 +0300

    Chat widgte

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -40,7 +40,6 @@ axiosInstance.interceptors.response.use(
   response => response,
   async (error) => {
     const originalRequest = error.config;
-
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
       console.log('Token expired, refreshing...');
